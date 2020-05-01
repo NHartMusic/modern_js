e6abee3b76de53d74b274795958f43b42977576c
Nick Hart <nhartmusic@gmail.com>
2020-05-01 23:12:11 +0200

    basic number manipulation methods

---
+++ numbers.js
@@ -0,0 +1,22 @@
+let radius = 10
+let pi = 3.14
+
+//modulo (remainder)
+
+let remainder = radius % 3
+
+let circle = pi * (radius ** 2)
+
+//REMEMBER BIDMAS #FUCKPEMDAS
+
+// Brackets -> Indices -> Division -> Multiplication -> Addition -> Subtraction 
+
+let bidmas_result = 5 * (10 - 3) ** 2
+
+let likes = 10
+likes += 59
+
+let nice = likes.toString() + ' nice.'
+
+console.log(nice)
+
