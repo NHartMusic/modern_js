67da85c0dba5b0e4487ae2e7eca484a9ccf534bc
Nick Hart <nhartmusic@gmail.com>
2020-05-02 23:27:33 +0200

    im drunk

---
+++ arrays.js
@@ -0,0 +1,18 @@
+let bigboys = ['tyler perry', 'steve austin', 'oprah winfrey']
+
+//array methods
+
+let squad = bigboys.join(' <===3 ')
+let index = bigboys.indexOf('steve austin')
+
+//does this count as destructive? 
+let catcat = bigboys.concat(['taylor swift', '50 Cent', 'Mike Pence'])
+
+// these ones are fsho destructive methods bruh 
+let kingpush = catcat.push('Post Malone')
+let lastboi = bigboys.pop()
+
+
+
+//let squadsquad = kingpush.join(' <===3 ')
+console.log(lastboi)
