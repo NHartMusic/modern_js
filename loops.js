9a16267e3287ff5a9608d2d9cc586db3c0d2794c
Nick Hart <nhartmusic@gmail.com>
2020-05-03 23:59:06 +0200

    jagered eyez, we're on the rise

---
+++ loops.js
@@ -0,0 +1,9 @@
+//for (let i = 0; i < 5; i++) {
+//    console.log('ratrat', i)
+//}
+
+const lads = ['Sean Hannity', 'Jill Biden', 'Cindy McCain', 'Marco Rubio']
+
+for (let i = 0; i < lads.length; i++) {
+    console.log(lads[i])
+}
