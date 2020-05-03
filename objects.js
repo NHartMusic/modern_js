1eeaead73b452ec6828f9645f7a781638accf58f
Nick Hart <nhartmusic@gmail.com>
2020-05-03 21:12:05 +0200

    hell yeah

--- objects.js
+++ objects.js
@@ -1,19 +1,22 @@
-//let tracks = [
-//    { id: 1, plays: 500, auto: false },
-//    { id: 2, plays: 1500, auto: false },
-//    { id: 3, plays: 700, auto: false },
-//    { id: 1, plays: 800, auto: false },
-//    { id: 4, plays: 900, auto: false },
-//    { id: 2, plays: 500, auto: false },
-//]
-
-
-const people = [
-    { name: 'Guy Fieri', age: 420 },
-    { name: 'Big Willy', age: 69 },
-    { name: 'Thicc Rick', age: 420 }
+const tracks = [
+    { id: 1, plays: 500, auto: false },
+    { id: 2, plays: 1500, auto: false },
+    { id: 3, plays: 700, auto: false },
+    { id: 1, plays: 800, auto: false },
+    { id: 4, plays: 900, auto: false },
+    { id: 2, plays: 500, auto: false },
 ]
 
-let blazeIt = people.filter(person => person.age === 420)
+let trackId = tracks.filter(song => song.id === 1)
 
-console.log(blazeIt)
+//const people = [
+//    { name: 'Guy Fieri', age: 420 },
+//    { name: 'Big Willy', age: 69 },
+//    { name: 'Thicc Rick', age: 420 }
+//]
+
+//let blazeIt = people.filter(person => person.age === 420)
+
+//let blazeItFirst = people.find(person => person.age === 420)
+
+console.log(trackId)
