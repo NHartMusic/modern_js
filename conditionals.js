fe83062620f54d73197b9eb3a3d293f940237647
Nick Hart <nhartmusic@gmail.com>
2020-05-04 17:16:05 +0200

    ok

--- conditionals.js
+++ conditionals.js
@@ -1,9 +1,31 @@
 const password = 'pl@ttRatt6669'
 
-if (password.length >= 12 && password.includes('@')) {
-    console.log('This password hard af bruh bruh')
-} else if (password.length >= 8 || password.includes('@') && password.length >= 5) {
-    console.log('This password ok I guess')
-} else {
-    console.log('This password fucking sucks dude')
+//if (password.length >= 12 && password.includes('@')) {
+//    console.log('This password hard af bruh bruh')
+//} else if (password.length >= 8 || password.includes('@') && password.length >= 5) {
+//    console.log('This password ok I guess')
+//} else {
+//    console.log('This password fucking sucks dude')
+//}
+
+const grade = 'B'
+
+switch (grade) {
+    case 'A':
+        alert('You fucking killed that shit bruh congrats')
+        break
+    case 'B':
+        console.log('Nice, solid effort')
+        break
+    case 'C':
+        console.log('Try harder next time')
+        break
+    case 'D':
+        console.log('Stop doing your homework while you are drunk')
+        break
+    case 'F':
+        alert('You are fucking trash don\'t ever come back to my fucking classroom again you horrible ungrateful smelly child')
+        break
+    default:
+        console.log('Do you think that god stays in heaven because he too, is afraid of what he has created?')
 }
