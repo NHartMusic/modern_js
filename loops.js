03aa7a2e6f369725a90250165c3c0a0a685ad060
Nick Hart <nhartmusic@gmail.com>
2020-05-04 16:25:54 +0200

    basic loop types

--- loops.js
+++ loops.js
@@ -4,6 +4,22 @@
 
 const lads = ['Sean Hannity', 'Jill Biden', 'Cindy McCain', 'Marco Rubio']
 
-for (let i = 0; i < lads.length; i++) {
-    console.log(lads[i])
-}
+//  for (let i = 0; i < lads.length; i++) {
+//      let text = `<div>${lads[i]}</div>`
+//      //console.log(text)
+//  }
+//  
+//  let rat = 0
+//  
+//  while (rat < 5) {
+//      console.log('in loop: ', rat)
+//      rat++
+//  }
+
+//do while 
+
+let platRat = 5
+
+do {
+    console.log('my rat is', 5)
+} while (platRat < 5)
